3416dceefc1bab95c9ae94e773f8ef323c2770f0
Drew Logsdon and Javier Pedemonte <pair+dalogsdo+pedemont@us.ibm.com>
2016-02-10 13:56:41 -0600

    Update to use `jupyter-js-widgets` as import
    Remove "npm compatibility" section, which we don't need.
    
    (c) Copyright IBM Corp. 2016

--- nb-extension/js/widgets/DeclWidgetModel.js
+++ nb-extension/js/widgets/DeclWidgetModel.js
@@ -1,34 +1,9 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-// npm compatibility
-function requireLocalFiles() {
-    require('../../../../../../node_modules/jupyter-js-widgets/static/widgets/js/manager-base');
-    require('../../../../../../node_modules/jupyter-js-widgets/static/widgets/js/widget');
-}
-
-if (typeof define !== 'function') {
-    var amdefine = require('amdefine')(module, require);
-
-    var mapping = {}
-    mapping['nbextensions/widgets/widgets/js/manager'] = '../../../../../../node_modules/jupyter-js-widgets/static/widgets/js/manager-base'
-    mapping['nbextensions/widgets/widgets/js/widget'] = '../../../../../../node_modules/jupyter-js-widgets/static/widgets/js/widget'
-
-    var define = function(){
-        var args = Array.prototype.slice.call(arguments);
-        if (args.length > 1) {
-            args[0] = args[0].map(function(arg) {
-                arg = mapping[arg] || arg;
-                return arg;
-            });
-        }
-        amdefine.apply(this, args);
-    }
-}
-
-define(["nbextensions/widgets/widgets/js/manager",
-    "nbextensions/widgets/widgets/js/widget"
-], function(widgetmanager, ipywidget) {
+define([
+    'jupyter-js-widgets'
+], function(Widgets) {
     "use strict";
 
     /**
@@ -36,10 +11,10 @@ define(["nbextensions/widgets/widgets/js/manager",
      */
 
     var DeclWidgetModel = function(widget_manager, model_id, comm) {
-        ipywidget.WidgetModel.apply(this, arguments);
+        Widgets.WidgetModel.apply(this, arguments);
     };
 
-    DeclWidgetModel.prototype = Object.create(ipywidget.WidgetModel.prototype);
+    DeclWidgetModel.prototype = Object.create(Widgets.WidgetModel.prototype);
 
     /*
      * Making request_state a noop to avoid requiring this handshake to take place to create model.
@@ -57,7 +32,7 @@ define(["nbextensions/widgets/widgets/js/manager",
         this.comm.send(data, callbacks);
     };
 
-    widgetmanager.ManagerBase.register_widget_model('DeclWidgetModel', DeclWidgetModel);
+    Widgets.ManagerBase.register_widget_model('DeclWidgetModel', DeclWidgetModel);
 
     return DeclWidgetModel;
 
